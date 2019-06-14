eded93d511da2caf55d237f3524a49a443ffb059
Anwar el Bouhdifi <anwar.elbouhdifi@gmail.com>
2019-06-14 11:32:38 +0200

    Small fix

--- src/Transaction/OtherCollection.php
+++ src/Transaction/OtherCollection.php
@@ -42,7 +42,7 @@ class OtherCollection implements TruckvisionCollectionInterface
                     'dos:OtherIdentifier' => $other->getOtherIdentifier(),
                     'dos:Quantity'        => $other->getQuantity()
                 ];
-            })
+            })->toArray()
         ];
     }
 
